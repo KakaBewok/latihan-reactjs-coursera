aa1042f0523b4b86683d5da5fa89e21e318ec6ef
kkbwk <rizalnov667@gmail.com>
2022-12-04 08:23:17 +0700

    exercise-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function greetings() {
+//   return (
+//     <div>
+//       <Navbar className="my-3" color="primary" dark>
+//         <div className="container">
+//           <NavbarBrand href="/">Noprizal</NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Menu dishes={this.state.dishes} />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className="my-3" color="primary" dark>
+          <div className="container">
+            <NavbarBrand href="/">Noprizal</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
