7dc944c0321130c338027e9c51d6de2603d506ed
kkbwk <rizalnov667@gmail.com>
2022-12-17 11:22:53 +0700

    SPA Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,26 +4,40 @@ import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
-    // const HomePage = () => {
-    //   return <Home />;
-    // };
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
 
     return (
       <div>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/contactus" element={<Contact />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
         </Routes>
         <Footer />
